33b2fbab3b0f729bdfa77afe3a03e395735d6ea1
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-09-01 20:40:03 -0500

    fix statics dir

--- millworkapp/millworkapp/settings.py
+++ millworkapp/millworkapp/settings.py
@@ -114,7 +114,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'statics'),
 ]
 
 django_heroku.settings(locals())
