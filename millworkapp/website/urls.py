3a7249b84c8f9b14b5d6d68881d0358404efa6d4
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-09-03 00:06:56 -0500

    separeted sections

--- millworkapp/website/urls.py
+++ millworkapp/website/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 
-from .views import about, contact
+
+from .views import about, contact, favicon
 
 
 urlpatterns = [
+    url(r'^favicon.ico$', favicon, name="favicon"),
     url(r'^contact$', contact, name='contact'),
     url(r'^about$', about, name='about'),
 ]
--- millworkapp/website/views.py
+++ millworkapp/website/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 # Create your views here.
+
+def favicon(request):
+    ''' Fix favicon '''
+    return redirect(settings.STATIC_URL + "brand/icons/favicon-32x32.png", permanent=True)
 
 def about(request):
     ''' About View'''
