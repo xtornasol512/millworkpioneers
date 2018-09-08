63eeb96fd93c71741f2f16c6f4b4de8ac6f4cc76
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-09-08 14:57:05 -0500

    custom admin

--- millworkapp/company/admin.py
+++ millworkapp/company/admin.py
@@ -1,12 +1,48 @@
 from django.contrib import admin
 
-from .models import Company, Client
+from .models import Company, Client, Quote
 
 class CompanyAdmin(admin.ModelAdmin):
     pass
 
 class ClientAdmin(admin.ModelAdmin):
-    pass
+    ''' Custom Client  admin '''
+    list_display = [
+        "name",
+        "website",
+        "headline",
+    ]
+    list_display_links = [
+        "name",
+        "website",
+        "headline",
+    ]
+
+class QuoteAdmin(admin.ModelAdmin):
+    ''' Custom Quote admin '''
+    list_display =[
+        'id',
+        'name',
+        'phone',
+        'email',
+        'company',
+        'project',
+        'start_date',
+        'created_at'
+    ]
+    list_display_links = [
+        'id',
+        'name',
+        'phone',
+        'email',
+        'company',
+        'project',
+        'start_date',
+        'created_at'
+
+    ]
+
 
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Client, ClientAdmin)
+admin.site.register(Quote, QuoteAdmin)
