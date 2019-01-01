0131d7c7f40c7f6f09e1937c6a7bc6cf97debd43
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-12-31 21:23:55 -0600

    add woodwork page settings

--- millworkapp/woodwork/admin.py
+++ millworkapp/woodwork/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.admin import ImageRenderAdmin
+from .models import WoodworkPage
+
+class WoodworkPageAdmin(ImageRenderAdmin, admin.ModelAdmin):
+    ''' Custom woddwork admin '''
+    list_display = [
+        'title',
+    ]
+
+admin.site.register(WoodworkPage, WoodworkPageAdmin)
