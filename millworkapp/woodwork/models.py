71e130729437c1475f704e14f8cef1d47788e35a
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2019-01-13 14:02:14 -0600

    add video page settings

--- millworkapp/woodwork/models.py
+++ millworkapp/woodwork/models.py
@@ -7,6 +7,7 @@ from core.behaviors import Timestampable
 class WoodworkPage(Timestampable, WebsiteModelPage, models.Model):
     ''' Page Model '''
     title = models.CharField("Page title",blank=True, max_length=255, default="Wodwork Page")
+    main_video = models.URLField("Youtube or vimeo URL", blank=True)
 
     class Meta:
         verbose_name_plural = "Woodwork Pioneers Page"
@@ -14,3 +15,8 @@ class WoodworkPage(Timestampable, WebsiteModelPage, models.Model):
 
     def __str__(self):
         return self.title
+
+    @property
+    def get_video_id(self):
+        ''' Get the ID for url link '''
+        return self.main_video.split("/")[-1]
