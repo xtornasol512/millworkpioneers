d68fdad55dfe3bbe3e0449297cfc2d29b70931b6
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2019-03-25 01:06:59 -0600

    send header logos if exists

--- millworkapp/millworkapp/views.py
+++ millworkapp/millworkapp/views.py
@@ -12,12 +12,17 @@ def home(request):
     reviews = Review.objects.all()[:REVIEW_PROJECTS]
     services = Service.objects.all()
     homepage = HomePage.objects.first()
+    if homepage is not None:
+        header_logos = homepage.page_header_photos.filter(is_display_on_website=True)
+    else:
+        header_logos = None
 
     context = {
-        "reviews" : reviews,
-        "services" : services,
-        "homepage" : homepage,
+        "reviews": reviews,
+        "services": services,
+        "homepage": homepage,
         "form": QuoteForm(),
+        "header_logos": header_logos,
     }
 
     return render(request, "website/home.html", context)
