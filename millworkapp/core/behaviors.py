08b94248b265ada5dcd0a437618253d535ffea02
Jesus Alvarado <xtornasol512@gmail.com>
2021-05-10 03:23:28 -0500

    no extra forms on header logos

--- millworkapp/core/behaviors.py
+++ millworkapp/core/behaviors.py
@@ -14,15 +14,15 @@ class Timestampable(models.Model):
 
 
 class PlayeableVideo(models.Model):
-	""" Share methods for manage video urls """
+    """ Share methods for manage video urls """
 
-	main_video = models.URLField("Youtube URL",
-					blank=True, default="https://youtu.be/CZb9XTONNkA",
-					help_text="Copy from youtube page")
+    main_video = models.URLField("Youtube URL",
+                        blank=True, default="https://youtu.be/CZb9XTONNkA",
+                        help_text="Copy from youtube page")
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
-	@property
-	def get_video_id(self):
-		return self.main_video.split("/")[-1]
+    @property
+    def get_video_id(self):
+        return self.main_video.split("/")[-1]
